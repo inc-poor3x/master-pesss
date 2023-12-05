b5990622a507cbce06f4173453b99778c051542e
vairos25 <osamaalhmadeen05@gmail.com>
2023-12-05 16:32:15 +0300

    database

--- API/master-pe.sql
+++ API/master-pe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2023 at 07:56 PM
+-- Generation Time: Dec 05, 2023 at 02:31 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,154 @@ SET time_zone = "+00:00";
 --
 -- Database: `master-pe`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `CartID` int(11) NOT NULL,
+  `ProductID` int(11) DEFAULT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL,
+  `Price` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `categoryID` int(11) NOT NULL,
+  `Category_name` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`categoryID`, `Category_name`) VALUES
+(2, 'Clothing'),
+(3, 'Books'),
+(4, 'Electronics');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `OrderID` int(11) NOT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `TotalPrice` int(11) DEFAULT NULL,
+  `OrderDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`OrderID`, `UserID`, `TotalPrice`, `OrderDate`) VALUES
+(3, 5, 1500, '2023-11-27 09:45:00'),
+(4, 6, 125, '2023-11-28 11:15:00'),
+(5, 5, 3900, '2023-12-04 14:48:26'),
+(6, 5, 3900, '2023-12-04 15:10:52'),
+(7, 5, 3250, '2023-12-04 15:38:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderitem`
+--
+
+CREATE TABLE `orderitem` (
+  `product` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL,
+  `Quantity` int(11) DEFAULT NULL,
+  `Price` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orderitem`
+--
+
+INSERT INTO `orderitem` (`product`, `OrderID`, `Quantity`, `Price`) VALUES
+(5, 3, 1, 600),
+(6, 4, 3, 75),
+(5, 6, 6, 650),
+(5, 7, 5, 650);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `ProductID` int(11) NOT NULL,
+  `categoryID` int(10) NOT NULL,
+  `StoreID` int(11) DEFAULT NULL,
+  `ProductName` varchar(255) DEFAULT NULL,
+  `Description` text DEFAULT NULL,
+  `Price` decimal(10,0) DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL,
+  `Image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`ProductID`, `categoryID`, `StoreID`, `ProductName`, `Description`, `Price`, `Quantity`, `Image`) VALUES
+(5, 2, 6, 'Updated Smartphone', 'Updated latest smartphone model', 650, 15, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `RatingID` int(11) NOT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `ProductID` int(11) DEFAULT NULL,
+  `RatingValue` int(11) DEFAULT NULL,
+  `Comment` text DEFAULT NULL,
+  `DateRated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`RatingID`, `UserID`, `ProductID`, `RatingValue`, `Comment`, `DateRated`) VALUES
+(7, 5, 5, 4, 'This is a great product!', '2023-12-03 18:47:23'),
+(8, 5, 5, 4, ' is a great product!', '2023-12-03 18:47:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `RoleID` int(11) NOT NULL,
+  `RoleName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`RoleID`, `RoleName`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, ' bus_account.');
 
 -- --------------------------------------------------------
 
@@ -41,13 +189,108 @@ CREATE TABLE `store` (
 --
 
 INSERT INTO `store` (`StoreID`, `UserID`, `StoreName`, `Category`, `StoreImage`, `IsActive`) VALUES
-(4, 4, 'Updated Store Name', 'Updated Category', NULL, '2'),
+(4, 4, 'Updated Store Name', 'Updated Category', 'path/to/your/new/image.jpg', '1'),
 (5, 5, 'Test User Store', 'Clothing', NULL, '2'),
-(6, 5, 'Test User Second Store', 'Books', NULL, '1');
+(6, 5, 'Test User Second Store', 'Books', NULL, '1'),
+(9, 5, 'Test Store', 'Electronics', 'path/to/your/image.jpg', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `successstory`
+--
+
+CREATE TABLE `successstory` (
+  `StoryID` int(11) NOT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `StoryText` text DEFAULT NULL,
+  `DatePublished` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `UserName` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `RoleID` int(11) DEFAULT 2,
+  `IsActive` enum('1','2') NOT NULL DEFAULT '2',
+  `imag` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`UserID`, `UserName`, `Password`, `Email`, `RoleID`, `IsActive`, `imag`) VALUES
+(1, 'newUsername', 'newPassword', 'newemail@example.com', 1, '1', 'new_image_url'),
+(4, 'newUsername', 'newPassword', 'newemail@example.com', 1, '1', 'new_image_url'),
+(5, 'test_user', 'user_password_test', 'user_test@example.com', 2, '2', 'user_image_test.jpg'),
+(6, 'test_guest', 'guest_password_test', 'guest_test@example.com', 2, '2', 'guest_image_test.jpg'),
+(9, 'osama_1', 'admin_password', 'admin@example.com', 2, '2', 'admin_image.jpg'),
+(13, 'osama_1', 'admin_password', 'admin@example.com', 2, '2', 'admin_image.jpg'),
+(17, 'taset_1', '$2y$10$YswcK41aWYzyHWwBeIHDYenotNE4aBQG0wer.thrGQcz9CxZh/JwO', 'testuser@example.com', 2, '1', 'testimage.jpg'),
+(18, '', '$2y$10$wgVLarw3MnDrvqkTs3NoFOaqeX22iPHMgTL8RtcqY6FB0Hqxm4qL6', '', 2, '2', NULL),
+(19, 'test_user', '$2y$10$UoTVIa014WBeIZJMp/WsPe7czg4pc.UGloT6rFF6N16tHA8J05wqm', 'test_user@example.com', 2, '2', NULL),
+(20, 'AHMAD', '$2y$10$FL6m1jzPtGJCfVf26lQ7d.h360jeKckc6h91jlLtWl5SDxrHBs2Qa', 'test_user@example.com', 2, '2', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`CartID`),
+  ADD KEY `ProductID` (`ProductID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryID`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD KEY `OrderID` (`OrderID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `StoreID` (`StoreID`),
+  ADD KEY `categoryID` (`categoryID`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`RatingID`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`RoleID`);
 
 --
 -- Indexes for table `store`
@@ -57,24 +300,131 @@ ALTER TABLE `store`
   ADD KEY `UserID` (`UserID`);
 
 --
+-- Indexes for table `successstory`
+--
+ALTER TABLE `successstory`
+  ADD PRIMARY KEY (`StoryID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`),
+  ADD KEY `RoleID` (`RoleID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `RatingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `RoleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `store`
 --
 ALTER TABLE `store`
-  MODIFY `StoreID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `StoreID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `successstory`
+--
+ALTER TABLE `successstory`
+  MODIFY `StoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD CONSTRAINT `orderitem_ibfk_2` FOREIGN KEY (`OrderID`) REFERENCES `order` (`OrderID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`StoreID`) REFERENCES `store` (`StoreID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`categoryID`) REFERENCES `category` (`categoryID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `rating`
+--
+ALTER TABLE `rating`
+  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `store`
 --
 ALTER TABLE `store`
   ADD CONSTRAINT `store_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `successstory`
+--
+ALTER TABLE `successstory`
+  ADD CONSTRAINT `successstory_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `store` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`RoleID`) REFERENCES `role` (`RoleID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
